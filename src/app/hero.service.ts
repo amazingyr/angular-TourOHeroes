f946a111f790b53736aaf7a7258550fee90d7a13
yr <amazing_yr@aliyun.com>
2022-01-27 01:12:12 +0800

    【英雄之路】
    Signed-off-by: yr <amazing_yr@aliyun.com>

---
+++ src/app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+// import { HeroesComponent } from './heroes/heroes.component';
+import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
+
+  constructor(private messageService: MessageService) { }
+}
